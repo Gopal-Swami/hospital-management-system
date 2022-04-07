2a644b7091f581842bd22f4f68631cf67f6cb50e
Gopal-Swami <Gopalswami8619@gmail.com>
2022-04-07 17:24:44 +0530

    Version 1.03
    Deployment fix 2

--- backend/server.js
+++ backend/server.js
@@ -22,18 +22,14 @@ app.use(express.json());
 app.use('/api/hospital', hospitalRoutes);
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running');
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('frontend/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
-
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
