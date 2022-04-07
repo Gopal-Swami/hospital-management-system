2f1d99d0dfe230b337b20c93e8ca2351447c0d65
Gopal-Swami <Gopalswami8619@gmail.com>
2022-04-07 17:59:12 +0530

    Version 1.04
    Deployment Fix

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import colors from 'colors';
 import morgan from 'morgan';
 import connectDB from './configurations/db.js';
-import path from 'path';
 import { errorHandler } from './middlewares/errorMiddleware.js';
 import hospitalRoutes from './routes/hospitalRoutes.js';
 
@@ -21,6 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use('/api/hospital', hospitalRoutes);
 app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('frontend/build'));
@@ -29,7 +29,6 @@ if (process.env.NODE_ENV == 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
-const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
