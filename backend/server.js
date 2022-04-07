4dc28ae8c980090903109cc25caf18b02edc0afb
Gopal-Swami <Gopalswami8619@gmail.com>
2022-04-07 18:03:41 +0530

    Version 1.05
    Deployment Fix 4

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import colors from 'colors';
+import path from 'path';
 import morgan from 'morgan';
 import connectDB from './configurations/db.js';
 import { errorHandler } from './middlewares/errorMiddleware.js';
@@ -24,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV == 'production') {
   app.use(express.static('frontend/build'));
-  const path = require('path');
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
