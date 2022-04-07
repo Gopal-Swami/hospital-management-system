9adde32cb3123fb957493e25442bacd8738d4b9b
Gopal-Swami <Gopalswami8619@gmail.com>
2022-04-07 18:09:41 +0530

    Version 1.05
    Small UI Fix for loader

--- frontend/src/screens/Bookings.jsx
+++ frontend/src/screens/Bookings.jsx
@@ -38,57 +38,60 @@ const Bookings = () => {
   }, [dispatch, success]);
   return (
     <>
-      {loading || resetLoading ? (
-        <Loader />
-      ) : (
-        <div className="booking-container">
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Room Type</th>
-                <th scope="col">Patient Name</th>
-                <th scope="col">Booking Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {bookings.map((booking, index) => (
-                <tr key={index + 1}>
-                  <th scope="row">{index + 1}</th>
-                  <td>
-                    {booking.room_type === 'normal_rooms'
-                      ? 'Normal Room'
-                      : booking.room_type === 'oxygen_rooms'
-                      ? 'Oxygen Room'
-                      : booking.room_type === 'icu_rooms'
-                      ? 'ICU Room'
-                      : 'NA'}
-                  </td>
-
-                  <td>{booking.patient_name}</td>
-                  <td>{booking.createdAt.substring(0, 10)}</td>
+      <div className="booking-container">
+        {loading ? (
+          <Loader />
+        ) : (
+          <>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Room Type</th>
+                  <th scope="col">Patient Name</th>
+                  <th scope="col">Booking Date</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <div className="booking-actions">
-            <button
-              type="button"
-              onClick={resetRoomAndBooking}
-              class="btn btn-danger m-2"
-            >
-              Delete All Bookings And Reset Room Availability
-            </button>
-            <button
-              onClick={(e) => navigate('/')}
-              type="button"
-              class="btn btn-dark m-2"
-            >
-              Back
-            </button>
-          </div>
-        </div>
-      )}
+              </thead>
+              <tbody>
+                {bookings.map((booking, index) => (
+                  <tr key={index + 1}>
+                    <th scope="row">{index + 1}</th>
+                    <td>
+                      {booking.room_type === 'normal_rooms'
+                        ? 'Normal Room'
+                        : booking.room_type === 'oxygen_rooms'
+                        ? 'Oxygen Room'
+                        : booking.room_type === 'icu_rooms'
+                        ? 'ICU Room'
+                        : 'NA'}
+                    </td>
+
+                    <td>{booking.patient_name}</td>
+                    <td>{booking.createdAt.substring(0, 10)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className="booking-actions">
+              <button
+                type="button"
+                onClick={resetRoomAndBooking}
+                class="btn btn-danger m-2"
+              >
+                Delete All Bookings And Reset Room Availability
+              </button>
+              <button
+                onClick={(e) => navigate('/')}
+                type="button"
+                class="btn btn-dark m-2"
+              >
+                Back
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+      )
     </>
   );
 };
