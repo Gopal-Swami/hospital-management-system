a91cffc99cdf7ddc02448ccea61cf034fcf930ec
Gopal-Swami <Gopalswami8619@gmail.com>
2022-04-07 17:00:27 +0530

    Version 1.01
    Updated Server file to deploy on heroku

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,18 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use('/api/hospital', hospitalRoutes);
 app.use(errorHandler);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
+
 const PORT = process.env.PORT;
 
 app.listen(
